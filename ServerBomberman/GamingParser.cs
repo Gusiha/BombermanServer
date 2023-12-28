a3f8cae87bf2d1301b4ab090181c8b8c1ce1b021
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2023-12-28 19:44:24 +0300

    (feat) parser added -added first parser version.

--- ServerBomberman/GamingParser.cs
+++ ServerBomberman/GamingParser.cs
@@ -1,13 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServerBomberman
+﻿namespace ServerBomberman
 {
-    internal class GamingParser
+    public class GamingParser
     {
+        static int[] Parse(string input)
+        {
+            string[] strings = input.Split(' ');
 
+            int[] response = new int[3];
+            for (int i = 0; i < response.Length; i++)
+            {
+                response[i] = 0;
+            }
+
+            if (strings.Length != 2)
+            {
+                response[2] = 400;
+                return response;
+            }
+
+            switch (strings[1])
+            {
+                case "left":
+                    {
+                        --response[0];
+                        break;
+                    }
+                case "right":
+                    {
+                        ++response[0];
+                        break;
+                    }
+                case "up":
+                    {
+                        --response[1];
+                        break;
+                    }
+                case "down":
+                    {
+                        ++response[1];
+                        break;
+                    }
+                case "place":
+                    {
+                        response[2] = 4;
+                        break;
+                    }
+                case "connect":
+                    {
+                        response[2] = 1;
+                        break;
+                    }
+                case "disconnect":
+                    {
+                        response[2] = 2;
+                        break;
+                    }
+            }
+
+            return response;
+        }
     }
 }
